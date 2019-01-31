8584813427bb2b2c2f37fb9403694be145b4d6e4
Alxander <onepeoplgood@gmail.com>
2019-01-31 19:02:37 +0200

    create portfolio section

--- js/custom.js
+++ js/custom.js
@@ -13,6 +13,16 @@ $(function(){
 
 });
 
-   
+$(function() {
+	 $('.tabs a').click(function() {
+		 $(this).parents('.tab-wrap').find('.tab-cont').addClass('hide');
+		 $(this).parent().siblings().removeClass('active');
+		 var id = $(this).attr('href');
+		 $(id).removeClass('hide');
+		 $(this).parent().addClass('active');
+		 return false;
+	 });
+})();
+
 
 
